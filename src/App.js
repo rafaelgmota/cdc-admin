0b896d6914ac6d0520d0630d2259aab913d87e0c
Rafael Mota <faelmota@gmail.com>
2017-12-02 12:40:43 -0300

    Cadastrando novos autores e atualizando componentes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/inputCustomizado';
 
 class App extends Component {
 
     constructor() {
         super();
-        this.state = {lista: []};
+        this.state = {lista: [], nome:'', email:'', senha:''};
+        this.enviaForm = this.enviaForm.bind(this);
+        this.setNome = this.setNome.bind(this);
+        this.setEmail = this.setEmail.bind(this);
+        this.setSenha = this.setSenha.bind(this);
     }
 
     //Called after the mount of component
@@ -23,6 +28,40 @@ class App extends Component {
                 this.setState({lista: resposta});
             }
         });
+    }
+
+    enviaForm(evento) {
+        evento.preventDefault();
+
+        $.ajax({
+            url:"http://localhost:8080/api/autores",
+            dataType:"json",
+            contentType:"application/json",
+            type:"post",
+            data:JSON.stringify({
+                nome: this.state.nome,
+                email: this.state.email,
+                senha: this.state.senha
+            }),
+            success: function(resposta) {
+                this.setState({lista: resposta});
+            }.bind(this),
+            error: function(resposta) {
+                console.log("Deu Errado Aí");
+            }
+        });
+    }
+
+    setNome(evento) {
+        this.setState({nome:evento.target.value});
+    }
+
+    setEmail(evento) {
+        this.setState({email:evento.target.value});
+    }
+
+    setSenha(evento) {
+        this.setState({senha:evento.target.value});
     }
 
   render() {
@@ -55,19 +94,10 @@ class App extends Component {
                 </div>
                 <div className="content" id="content">
                     <div className="pure-form pure-form-aligned">
-                        <form className="pure-form pure-form-aligned">
-                            <div className="pure-control-group">
-                                <label htmlFor="nome">Nome</label>
-                                <input id="nome" type="text" name="nome" value=""  />
-                            </div>
-                            <div className="pure-control-group">
-                                <label htmlFor="email">Email</label>
-                                <input id="email" type="email" name="email" value=""  />
-                            </div>
-                            <div className="pure-control-group">
-                                <label htmlFor="senha">Senha</label>
-                                <input id="senha" type="password" name="senha"  />
-                            </div>
+                        <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
+                            <InputCustomizado label="Nome" id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
+                            <InputCustomizado label="Email" id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
+                            <InputCustomizado label="Senha" id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
                             <div className="pure-control-group">
                                 <label></label>
                                 <button type="submit" className="pure-button pure-button-primary">Gravar</button>
